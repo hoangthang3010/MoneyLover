5213849549865c60e27c1f3464de3dcddc820434
hoangthang3010 <thangminhhoang98@gmail.com>
2023-04-25 14:29:43 +0700

    u

---
+++ src/components/Row.vue
@@ -0,0 +1,17 @@
+<template>
+  <tr>
+    <td>{{ row.active }}</td>
+    <td>{{ row.content }}</td>
+    <td>{{ row.date }}</td>
+    <td>{{ row.price }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Row",
+  props: ["row"],
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Sheet.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 "
+        >
+          <h2>VueSheet List</h2>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <a
+              href="https://docs.google.com/spreadsheets/d/1oLGI1u68sPh-P1yhWaU7j_WUTHjFMY1aUu6uINfTb0Q/edit?usp=sharing"
+              class="btn btn-sm btn-outline-secondary"
+              target="_blank"
+            >
+              View Google Sheet
+            </a>
+          </div>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-striped ">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Activity</th>
+                <th>City</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <Row v-bind:key="row.id" v-for="row in rows" v-bind:row="row" />
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Row from "@/components/Row.vue";
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const creds = require("@/client_secret.json");
+export default {
+  name: "Sheet",
+  components: {
+    Row,
+  },
+  props: ["sheet"],
+  data() {
+    return {
+      rows: [],
+      loading: true,
+    };
+  },
+  methods: {
+    async accessSpreadSheet() {
+      const doc = new GoogleSpreadsheet(
+        "1PETTwrEd_wt_12w_9MizACZ9dwW0oXr2J3EHmxlA3SM"
+      );
+      await doc.useServiceAccountAuth(creds);
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0];
+      const rows = await sheet.getRows({
+        offset: 1,
+      });
+      console.log(rows);
+      this.rows = rows;
+    },
+  },
+  created() {
+    this.accessSpreadSheet();
+  },
+};
+</script>
+
+<style scoped></style>
