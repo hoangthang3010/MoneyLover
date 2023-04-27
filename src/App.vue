3907b74b4b061651579b1db5954a49efa0883419
hoangthang3010 <thangminhhoang98@gmail.com>
2023-04-27 08:38:32 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
         <router-link class="navbar-brand col-sm-3 col-md-2 mr-0" to="/"
-          >VueSheet</router-link
+          >Thắng ♥ Nhài</router-link
         >
         <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
         <ul class="navbar-nav px-3">
